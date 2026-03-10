9afceec2d74aa9a2f4eebc4c89142ab3c1951dfc
Priyanka <aggarwalpriyanka45@gmail.com>
2026-03-10 12:32:33 +0530

    convert myapp from submodule to normal folder

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ border: "1px solid black" }}>
+        <h4>Todo List App</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
